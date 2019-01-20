d83ada119a98a8f8cfea141a342a3dd57c5179f5
Andres Acevedo - mran3 <x88andres@hotmail.com>
2019-01-20 18:14:55 -0500

    Initial Commit

---
+++ SegmentedViewControllers/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SegmentedViewControllers
+//
+//  Created by troquer on 20/01/2019.
+//  Copyright © 2019 acvc. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
